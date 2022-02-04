bfe91dcb46759aa38461abf01f5bfba63025f0f3
Lucio <info@freewebsolution.it>
2022-02-04 10:49:37 +0100

    Usando il debugger

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>My first App React Native to Lucio!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+console.log('App react Native');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
