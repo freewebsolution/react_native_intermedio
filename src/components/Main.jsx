12579efa199434cafa922e1feeb65f810c4bd277
Lucio <info@freewebsolution.it>
2022-02-09 12:52:49 +0100

    user interface coerente con il tema

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,15 @@
-import Constants from 'expo-constants';
-import { Text,StyleSheet,View } from 'react-native';
-import BigBlueText from './BigBlueText';
+import Text from './Text';
 
-const styles = StyleSheet.create({
-    container: {
-        marginTop:Constants.statusBarHeight,
-        flexGrow: 1,
-        flexShrink:1
-    }
-});
-const Main =() => {
-    return(
-         <View style={styles.container}>
-        <Text>Rate repository application</Text>
-        <BigBlueText/>
-    </View>   
+const Main = () => {
+    return (
+        <>
+        <Text>Simple text</Text>
+        <Text style={{paddingBottom: 10}}>Text with custom style</Text>
+            <Text fontWeight='bold' fontSize='subheading'>
+                Bold subheading
+            </Text>
+            <Text color='textSecondary'>Text with secondary color</Text>
+        </>
     )
 
 }
